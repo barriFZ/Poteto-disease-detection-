7d7e33c8ea27869c858d4dcb85955db41248e3cc
barriFZ <barri.fatima.zahrae@gmail.com>
2023-05-30 18:51:51 +0100

    initial commit

---
+++ api/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import os
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model_path = r"C:\Users\pc\Documents\dl_project\poteto_disease\models\1"
+
+if os.path.exists(model_path):
+    MODEL = tf.keras.models.load_model(model_path)
+    print("Model path  exist.")
+else:
+    print("Model path does not exist.")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+@app.get("/")
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8080)
